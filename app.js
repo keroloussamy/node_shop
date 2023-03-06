fb05e1f2f02836feab8ad7087ff92fe346493b09
keroloussamy <keroloussamy98@gmail.com>
2023-03-06 17:54:06 +0200

    Add product by mongodb

--- app.js
+++ app.js
@@ -4,29 +4,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   // User.findById(1)
+//   //   .then(user => {
+//   //     req.user = user;
+//   //     next();
+//   //   })
+//   //   .catch(err => console.log(err));
+//   next();
+// });
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
