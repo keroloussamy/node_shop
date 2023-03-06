b177ce6a88ec197ee5e2a2ec4af91aeed838513e
keroloussamy <keroloussamy98@gmail.com>
2023-03-06 16:44:05 +0200

    Add mongodb connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,32 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://kero:1234@cluster0.2eous.mongodb.net/node-shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw new Error('No database found!');
+};
+
+module.exports = { mongoConnect, getDb };
+
+
+// Another way to export A function:
+// exports.mongoConnect = mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
